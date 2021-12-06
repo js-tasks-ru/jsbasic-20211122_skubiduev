22449325fdd8d55a8f12376005e2ff3acf0148fa
skubiduev <skubiduev@rambler.ru>
2021-12-06 21:43:27 +0300

    ''

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
   // ваш код...
+  let a = table.rows;
+  for (let b of a) {
+    if (b.cells[3].getAttribute('data-available') === 'true') {
+      b.classList.add('available');
+    } else if (b.cells[3].getAttribute('data-available') === 'false') {
+      b.classList.add('unavailable');
+    } else {
+      b.setAttribute('hidden', 'hidden');
+    }
+
+    if (b.cells[2].textContent === 'm') {
+      b.classList.add('male');
+    } else {
+      b.classList.add('female');
+    }
+
+    if (+b.cells[1].textContent < 18) {
+      b.style.textDecoration = 'line-through';
+    }
+  }
 }
